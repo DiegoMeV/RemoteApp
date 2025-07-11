10be95d2c5652f0337722258e3a80b0551e3d98e
DiegoMeV <diegom.solinfo@gmail.com>
2025-07-11 08:26:49 -0500

    Ajuste de env

--- vite.config.js
+++ vite.config.js
@@ -1,80 +1,84 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import federation from '@originjs/vite-plugin-federation'
 
-export default defineConfig({
-  server: {
-    port: 3001,
-    open: true,
-  },
-  plugins: [
-    react(),
-    federation({
-      name: 'remote-app',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './Roles': './src/pages/administration/roles/Roles.jsx',
-        './AplicationsRoutes': './src/containers/routes/applications/ApplicationsRoutes.jsx',
-      },
-      remotes: {
-        'host-app': 'http://localhost:3000/assets/remoteEntry.js',
-      },
-      shared: {
-        react: {
-          singleton: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      port: 3001,
+      open: true,
+    },
+    plugins: [
+      react(),
+      federation({
+        name: 'remote-app',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './Roles': './src/pages/administration/roles/Roles.jsx',
+          './AplicationsRoutes': './src/containers/routes/applications/ApplicationsRoutes.jsx',
         },
-        'react-dom': {
-          singleton: true,
+        remotes: {
+          'host-app': `${env.VITE_ADMIN_MF}/assets/remoteEntry.js`,
         },
-        'easy-peasy': {
-          singleton: true,
+        shared: {
+          react: {
+            singleton: true,
+          },
+          'react-dom': {
+            singleton: true,
+          },
+          'easy-peasy': {
+            singleton: true,
+          },
+          zustand: {
+            singleton: true,
+          },
+          '@tanstack/react-query': {
+            singleton: true,
+          },
+          'react-router-dom': {
+            singleton: true,
+          },
+          tailwindcss: {
+            singleton: true,
+          },
+          '@mui/material': {
+            singleton: false,
+          },
+          '@mui/icons-material': {
+            singleton: false,
+          },
+          '@emotion/react': {
+            singleton: false,
+          },
+          '@emotion/styled': {
+            singleton: false,
+          },
         },
-        zustand: {
-          singleton: true,
+      }),
+    ],
+    build: {
+      target: 'esnext',
+    },
+    esbuild: {
+      loader: 'jsx',
+      include: /.*\.jsx?$/, // Incluye archivos .js y .jsx
+      exclude: [],
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx',
         },
-        '@tanstack/react-query': {
-          singleton: true,
-        },
-        'react-router-dom': {
-          singleton: true,
-        },
-        tailwindcss: {
-          singleton: true,
-        },
-        '@mui/material': {
-          singleton: false,
-        },
-        '@mui/icons-material': {
-          singleton: false,
-        },
-        '@emotion/react': {
-          singleton: false,
-        },
-        '@emotion/styled': {
-          singleton: false,
-        },
-      },
-    }),
-  ],
-  build: {
-    target: 'esnext',
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /.*\.jsx?$/, // Incluye archivos .js y .jsx
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
+  }
 })
