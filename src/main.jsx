ce3856a0162b88452f7846bae797d9bdd08bccd1
DiegoMeV <diegom.solinfo@gmail.com>
2025-07-11 08:50:55 -0500

    Ajuste de estilos

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import RouterRoutes from './containers/routes/RouterRoutes.jsx'
 import MainContainer from './containers/mainContainer/MainContainer.jsx'
 import './index.css'
-import('host-app/globalStyles') // Importing the QueryClient from the host app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
